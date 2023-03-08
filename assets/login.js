fff7c6ac7ae67db256f4dfc3121016fb72dbb8f2
Aurélien Ruano <aurelien.ruano@epitech.eu>
2023-03-08 11:23:34 +0900

    fix button facebook login

--- assets/login.js
+++ assets/login.js
@@ -31,7 +31,7 @@ window.fbAsyncInit = function() {
   });
 
   FB.AppEvents.logPageView();   
-
+  document.dispatchEvent(new Event('fbloaded'));
 };
 
 function checkLoginState() {
@@ -41,21 +41,24 @@ function checkLoginState() {
 }
 window.checkLoginState = checkLoginState;
 
-(function(d, s, id){
- var js, fjs = d.getElementsByTagName(s)[0];
- if (d.getElementById(id)) {return;}
- js = d.createElement(s); js.id = id;
- js.src = "https://connect.facebook.net/en_US/sdk.js";
- fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+document.addEventListener('DOMContentLoaded', function() {
+  document.addEventListener('fbloaded', () => {
+    (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
 
-FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
+    FB.getLoginStatus(function(response) {
+        statusChangeCallback(response);
+    });
+    var finished_rendering = function() {
+      console.log("finished rendering plugins");
+      var spinner = document.getElementById("spinner");
+      spinner.remove();
+    }
+    FB.Event.subscribe('xfbml.render', finished_rendering);
+  });
 });
-var finished_rendering = function() {
-  console.log("finished rendering plugins");
-  var spinner = document.getElementById("spinner");
-  spinner.removeAttribute("style");
-  spinner.removeChild(spinner.childNodes[0]);
-}
-FB.Event.subscribe('xfbml.render', finished_rendering);
