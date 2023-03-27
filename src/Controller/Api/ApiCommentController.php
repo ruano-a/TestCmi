ff0b2c371c59bbcb1a7c1d618553b54e4272b8e0
Aurélien Ruano <aurelien.ruano@epitech.eu>
2023-03-27 19:09:56 +0900

    fix limit query

--- src/Controller/Api/ApiCommentController.php
+++ src/Controller/Api/ApiCommentController.php
@@ -20,7 +20,7 @@ class ApiCommentController extends BaseApiController
     #[Route('/recent', name: 'get_recent_comments')]
     public function getRecentComments(CommentRepository $commentRepository, CommentService $commentService): Response
     {
-        return $this->serializedResponse($commentRepository->findBy([], ['creationDate' => 'desc']), 'read-recent-comments', 50);
+        return $this->serializedResponse($commentRepository->findBy([], ['creationDate' => 'desc'], 50), 'read-recent-comments');
     }
 
     #[IsGranted('IS_AUTHENTICATED_REMEMBERED')]
